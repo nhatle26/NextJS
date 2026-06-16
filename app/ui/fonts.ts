692ab3545b88d78c23c2bb986efa1a7b3caa487f
Lê Minh Nhật <leminhnhat1326@gmail.com>
2026-06-16 14:48:54 +0700

    Inital commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Roboto_Condensed } from 'next/font/google';
+
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const myFont = Roboto_Condensed({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});
